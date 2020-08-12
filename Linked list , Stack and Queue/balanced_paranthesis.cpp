68103e641fd9dd29ae5a2c44d494e67c9aabb91c
abhisheknaw <62641161+abhisheknaw@users.noreply.github.com>
2020-08-12 11:02:01 +0530

    Add files via upload

---
+++ Linked list , Stack and Queue/balanced_paranthesis.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool arepair(char opening,char closing)
+{
+	if(opening=='(' && closing==')')
+	{
+		return true;
+	}
+	else if(opening=='{' && closing=='}')
+	{
+		return true;
+	}
+	else if(opening=='[' && closing==']')
+	{
+		return true;
+	}
+	return false;
+}
+bool areparanthesesbalanced(string exp)
+{
+	int i;
+	stack<char>s;
+	for(i=0;i<strlen(exp.c_str());i++)
+	{
+		if(exp[i]=='(' || exp[i]=='{' || exp[i]=='[')
+		{
+			s.push(exp[i]);
+		}
+		else if(exp[i]==')' || exp[i]=='}' || exp[i]==']')
+		{	
+			if(s.empty() || !arepair(s.top(),exp[i]))
+			{
+				return false;
+		    }
+		    else
+		    {
+		    	s.pop();
+		    }	
+		}
+	}
+	return s.empty() ? true:false;
+}
+
+int main() 
+{
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	// for getting input from input.txt
+	freopen("input.txt", "r", stdin);
+	// for writing output to output.txt
+	freopen("output.txt", "w", stdout);
+#endif
+	string exp;
+	cin>>exp;
+	if(areparanthesesbalanced(exp))
+		cout<<"balanced";
+	else
+		cout<<"not balanced";
+	return 0;
+}	
